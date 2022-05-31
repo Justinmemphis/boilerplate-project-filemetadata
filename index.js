096b7e3314757a5a0e4c7c5e13b4752c43aa8723
Justin Carter <63558266+Justinmemphis@users.noreply.github.com>
2022-05-31 16:37:10 -0500

    posting finished project

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// freecodecamp template below
+
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
@@ -11,10 +13,21 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
-
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
+
+// freecodecamp template above
+
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  console.log(req.file, req.body)
+  return res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+})
